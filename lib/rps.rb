2df5f39b4f6335ed6c1c4213c14fc90ca0e2797c
squarebe <squarebe03@gmail.com>
2015-08-26 12:53:02 +0100

    now we can see the player name

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
-    'Hello RPS!'
+    erb :index
+  end
+
+  get '/play_game' do
+    @name = params[:name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
